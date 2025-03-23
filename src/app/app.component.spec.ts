f79a43252bb00f384bce74fc1e356a6a74af431d
A B M Asadullah <abmasadullah@gmail.com>
2025-03-23 21:01:21 +0600

    Refactor code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'studio-booking-app' title`, () => {
+  it(`should have the 'studio-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('studio-booking-app');
+    expect(app.title).toEqual('studio-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, studio-booking-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, studio-app');
   });
 });
--- src/app/components/studio-list/studio-list.component.ts
+++ src/app/components/studio-list/studio-list.component.ts
@@ -1,42 +1,11 @@
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component } from '@angular/core';
-import { StudioService } from '../../services/studio.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FilterPipe } from '../../filter.pipe';
-import { AppointmentFormComponent } from "../appointment-form/appointment-form.component";
 
 @Component({
   selector: 'app-studio-list',
-  imports: [CommonModule, FormsModule, FilterPipe, AppointmentFormComponent],
+  imports: [],
   templateUrl: './studio-list.component.html',
   styleUrl: './studio-list.component.css'
 })
-
 export class StudioListComponent {
-  dataList: any = [];
-  searchText: any;
 
-  constructor(
-    private studioService: StudioService,     
-    private modalService: NgbModal,
-  ) { }
-
-
-  bookNow(bookNowPopUp: any) {
-    this.modalService.open(bookNowPopUp, {ariaLabelledBy: 'modal-basic-title', size: 'lg'});
-  }
-
-  ngOnInit(): void {
-    this.loadData();
-  }
-
-  loadData() {
-    this.studioService.getData().subscribe((data) => {
-      this.dataList = JSON.parse(JSON.stringify(data.Studios)).map((item: any) => {
-        return item;
-      });
-      console.log(this.dataList[10]);
-    });
-  }
 }
